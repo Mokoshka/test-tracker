e711fa4853430f69f1b1621f57d3168992c07c90
Mokoshka <mokosha95@gmail.com>
2019-03-15 18:09:44 +0500

    Добавила порт для Хероку

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.get('/', jsonParser, function (req, res) {
     res.sendStatus(200);
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log('Example app listening on port 3000!');
 });
